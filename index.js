1b4cc1d4e3fd38f60cd68d3a79400800df37abf7
Constanza Díaz <constanza.diaz@gmail.com>
2019-05-20 13:03:05 +0200

    printing user details on console

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-
+console.log
 //function getUser(name){
   var req = new XMLHttpRequest();
   var name = "marfeel";
   var url="https://api.github.com/users/"+name;
   req.open("GET", url, true);
 
-  console.log(req.readyState);
-  console.log(req);
+  req.onreadystatechange = function(){
+    if (req.readyState == 4) {
+      if(req.status == 200 && req.response != "") {
+        if(req.status == 200){
+         //console.log("response"+req.response);
+         const parsedResponse = JSON.parse(req.response);
+         const loginName = parsedResponse["login"];
+         const fullName = parsedResponse["name"];
+         const description = parsedResponse["bio"];
+         const reposUrl = parsedResponse["repos_url"];
 
-  req.oneadystatechange = function(){
-    if(req.status == 200 && req.response != "") {
+         console.log("login: "+loginName);
+         console.log("name: "+fullName);
+         console.log("bio: "+description);
+         console.log("repos_url: "+reposUrl);
+
+
+       }else{
+         console.log("Error loading page");
+        }
+      }
     }
   }
   req.send();
+
 
 
 //}
