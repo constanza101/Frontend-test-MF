de8b45910f73ccaf057e0a8501f4f1d443a3efdc
Constanza Díaz <constanza.diaz@gmail.com>
2019-05-21 16:39:01 +0200

    app is working correctly

--- index.js
+++ index.js
@@ -1,122 +1,107 @@
-const searchInputForm = document.getElementById('searchForm');
 const resultsDiv = document.querySelector(".results");
 const noResultsDiv = document.querySelector(".noResults");
-resultsDiv.style.display = "none"
-noResultsDiv.style.display = "none"
+const searchForm = document.getElementById('searchForm');
 
-
-//document.querySelector(".repositories").style.display = "none"
-document.querySelector(".results").style.display = "none"
-
-searchInputForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 });
 
 const searchButtonElement = document.getElementById('searchButton');
 
 searchButtonElement.addEventListener('click', function(event) {
-  search();
+    search();
 });
 
 function search() {
-  const userName = document.getElementById('searchInput').value;;
-  const urlRepos = "https://api.github.com/users/" + userName + "/repos";
-
-  getUser(userName, function(parsedResponse) {
-    cleanForm();
-    printUserDetails(parsedResponse);
-    searchRepos(urlRepos)
-  }, function(error) {
-    showError();
-    console.log("getUser error");
-
-  });
+    const userName = document.getElementById('searchInput').value;;
+    const urlRepos = "https://api.github.com/users/" + userName + "/repos";
+    if (userName === "") {
+        cleanForm();
+    } else if (userName != "") {
+        getUser(userName, function(userData) {
+            printUserDetails(userData);
+            searchRepos(urlRepos)
+        }, function(error) {
+            showError();
+        });
+    }
 }
 
 function getUser(userName, success, error) {
-  var urlUser="https://api.github.com/users/" + userName;
-  var req = new XMLHttpRequest();
+    const urlUser = "https://api.github.com/users/" + userName;
+    const req = new XMLHttpRequest();
+    cleanForm();
 
-  req.open("GET", urlUser, true);
-  req.onreadystatechange = function(){
-    if (req.readyState === 4) {
-      if (req.status === 200) {
-          const parsedResponse = JSON.parse(req.response);
+    req.open("GET", urlUser, true);
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                const userData = JSON.parse(req.response);
+                success(userData);
+            }
 
-          success(parsedResponse);
-      }
-
-      if (req.status === 404) {
-        console.log('User not found');
-        error();
-      }
+            if (req.status === 404) {
+                error();
+            }
+        }
     }
-  }
 
-  req.send();
+    req.send();
 } //getUser
-
-function searchRepos(urlRepos){
-var req = new XMLHttpRequest();
-
-req.open("GET", urlRepos, true);
-req.onreadystatechange = function(){
-  if (req.readyState === 4) {
-    if (req.status === 200) {
-        const parsedResponse = JSON.parse(req.response);
-
-        printRepos(parsedResponse);
-    }
-  }
-}
-req.send();
-} //fin-searchRepos()
-
 
 
 function printUserDetails(data) {
-   const loginName = data["login"];
-   const fullName = data["name"];
-   const description = data["bio"];
-   const avatarUrl = data["avatar_url"]
-   document.querySelector(".loginName").innerText = "@"+loginName;
-   document.querySelector(".fullName").innerText = fullName;
-   document.querySelector(".description").innerText = description;
-   document.querySelector("img").src = avatarUrl;
-   document.querySelector("img").alt = loginName;
+    const loginName = data["login"];
+    const fullName = data["name"];
+    const description = data["bio"];
+    const avatarUrl = data["avatar_url"]
+    document.querySelector(".loginName").innerText = "@" + loginName;
+    document.querySelector(".fullName").innerText = fullName;
+    document.querySelector(".description").innerText = description;
+    document.querySelector("img").src = avatarUrl;
+    document.querySelector("img").alt = loginName;
 }
+
+function searchRepos(urlRepos) {
+    const req = new XMLHttpRequest();
+
+    req.open("GET", urlRepos, true);
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                const userRepos = JSON.parse(req.response);
+
+                printRepos(userRepos);
+            }
+        }
+    }
+    req.send();
+} //fin-searchRepos()
+
 
 function printRepos(repos) {
-  document.querySelector(".results").style.display = "block"
+    const starImg = '<img src="src/star.svg" alt="star">';
+    const forkImg = '<img src="src/fork.svg" alt="forks">';
+    const tableElement = document.querySelector("table");
 
-  const starImg = '<img src="src/star.svg" alt="star">';
-  const forkImg = '<img src="src/fork.svg" alt="forks">';
-  const tableElement = document.querySelector("table");
+    tableElement.innerHTML = ''; // Clean up the table content.
+    resultsDiv.style.display = "block"
 
+    for (let i = 0; i < repos.length; i++) {
+        console.log(repos[i]["name"]);
+        let repoName = repos[i]["name"];
+        let stars = repos[i]["stargazers_count"];
+        let forks = repos[i]["forks_count"];
 
-  tableElement.innerHTML = ''; // Clean up the table content.
-
-  for (var i = 0; i < repos.length; i++) {
-    console.log(repos[i]["name"]);
-    let repoName = repos[i]["name"];
-    let stars = repos[i]["stargazers_count"];
-    let forks = repos[i]["forks_count"];
-
-    console.log(repoName, stars, forks);
-
-    tableElement.innerHTML += '<tr><th scope="row">' + repoName + '</th><td>' + starImg + ' ' + stars + ' ' + forkImg + ' ' + forks + '</td></tr>';
-  }
+        tableElement.innerHTML += '<tr><th scope="row">' + repoName + '</th><td>' + starImg + ' ' + stars + ' ' + forkImg + ' ' + forks + '</td></tr>';
+    }
 }
 
-function showError(asd) {
-  console.log("show error");
-  cleanForm()
-  noResultsDiv.style.display = "block"
-
-
+function showError() {
+    noResultsDiv.style.display = "block";
 }
 
 function cleanForm() {
-  resultsDiv.style.display = "none"
-  noResultsDiv.style.display = "none"
+    resultsDiv.style.display = "none";
+    noResultsDiv.style.display = "none";
 }
