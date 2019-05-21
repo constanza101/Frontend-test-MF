aff9c0e4ffc830a60bcd2cdacb66f6010fee04ee
Constanza Díaz <constanza.diaz@gmail.com>
2019-05-21 23:56:52 +0200

    formatted

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const resultsDiv = document.querySelector(".results");
-const noResultsDiv = document.querySelector(".noResults");
-const searchForm = document.getElementById('searchForm');
+const RESULTS_DIV = document.querySelector(".results");
+const NO_RESULTS_DIV = document.querySelector(".noResults");
+const SEARCH_FORM = document.getElementById('searchForm');
+const GITHUB_USERS = "https://api.github.com/users/";
 
-searchForm.addEventListener('submit', function(event) {
+SEARCH_FORM.addEventListener('submit', function(event) {
     event.preventDefault();
 });
+
+/**
+ * Takes the value of the form input = login name of a Github user
+ */
 
 const searchButtonElement = document.getElementById('searchButton');
 
@@ -12,23 +17,37 @@ searchButtonElement.addEventListener('click', function(event) {
     search();
 });
 
+
+/**
+ * Takes the value of the form input = login name of a Github user
+ */
+
 function search() {
     const userName = document.getElementById('searchInput').value;;
-    const urlRepos = `https://api.github.com/users/${userName}/repos`;
+    const urlRepos = `${GITHUB_USERS}${userName}/repos`;
     if (userName === "") {
         cleanForm();
     } else if (userName != "") {
         getUser(userName, function(userData) {
             printUserDetails(userData);
-            searchRepos(urlRepos)
+            searchRepos(urlRepos);
         }, function(error) {
             showError();
         });
     }
 }
 
+
+/**
+ * Searches an user on Github based on user name.
+ *
+ * @param {string} userName - User name from the input value, to be searched on Github.
+ * @param {function} success - Callback when the fetch is successful.
+ * @param {function} error - Callback when the fetch fails.
+ */
+
 function getUser(userName, success, error) {
-    const urlUser = `https://api.github.com/users/${userName}`;
+    const urlUser = `${GITHUB_USERS}${userName}`;
     const req = new XMLHttpRequest();
     cleanForm();
 
@@ -47,20 +66,29 @@ function getUser(userName, success, error) {
     }
 
     req.send();
-} //getUser
+}
 
+/**
+ * Prints user's personal info from the users endpoint.
+ * @param {object} data - user information.
+*/
 
 function printUserDetails(data) {
     const loginName = data["login"];
     const fullName = data["name"];
     const description = data["bio"];
-    const avatarUrl = data["avatar_url"]
+    const avatarUrl = data["avatar_url"];
     document.querySelector(".loginName").innerText = `@${loginName}`;
     document.querySelector(".fullName").innerText = fullName;
     document.querySelector(".description").innerText = description;
     document.querySelector("img").src = avatarUrl;
     document.querySelector("img").alt = loginName;
 }
+
+/**
+ * searches user's repositories  from the repositories endpoint.
+ * @param {string} urlRepos - user's repositories endpoint url.
+*/
 
 function searchRepos(urlRepos) {
     const req = new XMLHttpRequest();
@@ -76,8 +104,13 @@ function searchRepos(urlRepos) {
         }
     }
     req.send();
-} //fin-searchRepos()
+}
 
+
+/**
+ * Prints a list of user's repositories from the repos endpoint - with stars and forks for each repo.
+ * @param {object} repos - user information.
+*/
 
 function printRepos(repos) {
     const starImg = '<img src="src/star.svg" alt="star">';
@@ -85,7 +118,7 @@ function printRepos(repos) {
     const tableElement = document.querySelector("table");
 
     tableElement.innerHTML = ''; // Clean up the table content.
-    resultsDiv.style.display = "block"
+    RESULTS_DIV.style.display = "block";
 
     for (let i = 0; i < repos.length; i++) {
         console.log(repos[i]["name"]);
@@ -101,11 +134,19 @@ function printRepos(repos) {
     }
 }
 
+/**
+  * Displays the "user not found" result.
+*/
+
 function showError() {
-    noResultsDiv.style.display = "block";
+    NO_RESULTS_DIV.style.display = "block";
 }
 
+/**
+  *  hides all previous results.
+*/
+
 function cleanForm() {
-    resultsDiv.style.display = "none";
-    noResultsDiv.style.display = "none";
+    RESULTS_DIV.style.display = "none";
+    NO_RESULTS_DIV.style.display = "none";
 }
